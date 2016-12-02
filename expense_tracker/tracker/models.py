233a47b9c267658fc5c6d1812c820d1a17de7ebe
Yusuf Musleh <yusuf.musleh@gmail.com>
2016-12-02 00:33:08 -0800

    Added Expense to Models. Added create new expense form fields

--- expense_tracker/tracker/models.py
+++ expense_tracker/tracker/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+	owner = models.ForeignKey(User, on_delete=models.CASCADE)
+	date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+	amount = models.DecimalField(max_digits=19, decimal_places=2)
+	description = models.TextField()
+
+	def __unicode__(self):
+		return self.amount + " spent on " + self.description + " by " +  self.owner
